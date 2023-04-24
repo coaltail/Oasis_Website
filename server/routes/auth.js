6ae61af572a51dea737c33944aaeb1be67916136
Luka <coaltail420@gmail.com>
2023-04-24 15:11:56 +0200

    Htpp cookies and refresh tokens

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyUserWithToken } from "../middleware/auth.js";
-import { loginUser, registerNewUser, loginAdmin } from "../controllers/auth.js"
+import { loginUser, registerNewUser, loginAdmin, refreshToken } from "../controllers/auth.js"
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerNewUser);
-router.post("/login/__admin__", loginAdmin);
+router.post("/admin/login", loginAdmin);
+router.get("/refresh", refreshToken);
 
 export default router;

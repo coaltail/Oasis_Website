9a1187c778b68ecfeb36d466179c584e01696a1e
Luka <lbaksa22@student.foi.hr>
2023-05-12 12:53:01 +0200

    Separating backend and frontend

--- server/routes/order.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createNewOrder } from '../controllers/orderController.js';
-import { verifyUserWithToken } from '../middleware/auth.js';
-const router = express.Router();
-
-router.post("/create", verifyUserWithToken, createNewOrder);
-
-export default router;

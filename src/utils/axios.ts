af9db4cb9e7d23ccf48a4f6c8407998cddc6fb1a
Luka <lbaksa22@student.foi.hr>
2023-05-12 18:49:37 +0200

    asd

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://oasis-website-ej7yini5o-coaltail.vercel.app/',
+  baseURL: 'https://oasis-website-git-main-coaltail.vercel.app/',
   timeout: 5000,
   withCredentials: true,
 });
